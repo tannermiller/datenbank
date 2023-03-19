f91c727570cdffd4f3eabaa32fa5a732a198ed2a
Tanner Miller <tannermiller@gmail.com>
2023-03-19 18:53:15 -0400

    Add integration test

--- datenbank_core/tests/integration.rs
+++ datenbank_core/tests/integration.rs
@@ -3,6 +3,7 @@ use datenbank_core::Database;
 #[test]
 fn test_create_table() {
     let mut db = Database::memory();
+
     let result = db
         .exec(
             r"CREATE TABLE testing {
@@ -13,4 +14,22 @@ fn test_create_table() {
         )
         .unwrap();
     assert_eq!(0, result.rows_affected);
+
+    // insert in order
+    let result = db
+        .exec("INSERT INTO testing (foo, bar, baz) VALUES (1, false, \"hello\")")
+        .unwrap();
+    assert_eq!(1, result.rows_affected);
+
+    // insert columns out of order
+    let result = db
+        .exec("INSERT INTO testing (baz, foo, bar) VALUES (\"world\", 2, true)")
+        .unwrap();
+    assert_eq!(1, result.rows_affected);
+
+    // insert multiple
+    let result = db
+        .exec("INSERT INTO testing (foo, bar, baz) VALUES (3, false, \"whats\"), (4, true, \"up\")")
+        .unwrap();
+    assert_eq!(2, result.rows_affected);
 }
